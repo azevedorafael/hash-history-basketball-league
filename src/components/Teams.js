2c5f98fbb2cb53d695bf75b531598dbf68bae531
azevedorafael <rafaelsilva.fatec@gmail.com>
2018-11-21 22:58:57 -0200

    Initial routes

---
+++ src/components/Teams.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Teams extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className='container two-column'>
+                Teams
+            </div>
+        );
+    }
+}
+
+Teams.propTypes = {
+
+};
+
+export default Teams;
