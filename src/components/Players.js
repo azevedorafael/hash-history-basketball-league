f720c38a131492ee0bf20c8a916bb067c5ad9230
azevedorafael <rafaelsilva.fatec@gmail.com>
2018-11-25 20:20:11 -0200

    Fetching the players

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
+import Sidebar from './Sidebar'
+import { getPlayers } from '../api'
+import { parse } from 'query-string'
 
 class Players extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    componentWillMount() {
-
+    state = {
+        players: [],
+        loading: true,
     }
 
     componentDidMount() {
+        const { location } = this.props
+
+        location.search
+            ? this.fetchPlayers(parse(location.search).teamId)
+            : this.fetchPlayers()
 
     }
 
-    componentWillReceiveProps(nextProps) {
-
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
-    }
-
-    componentWillUnmount() {
-
+    fetchPlayers = (teamId) => {
+        getPlayers(teamId)
+            .then((players) => this.setState(() => ({
+                loading: false,
+                players
+            })))
     }
 
     render() {
+        const { players, loading } = this.state
+        const { match, location } = this.props
+
         return (
             <div className='container two-column'>
-                Players
+                <Sidebar
+                    loading={loading}
+                    title='Players'
+                    list={players.map((player) => player.name)}
+                    {...this.props}
+                />
+
+                {loading === false && location.pathname === '/players'
+                    ? <div className='sidebar-instruction'>Select a player</div>
+                    : null
+                }
             </div>
         );
     }
